cec31e9b5410e7f98b1ca953680294dcd9edd72e
shansen5 <steven.e.hansen@gmail.com>
2017-04-19 23:03:04 -0700

    Add changes to server.js.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require( 'path' );
+const http = require( 'http' );
 const express = require( 'express' );
+const socketIO = require( 'socket.io' );
 
 const publicPath = path.join( __dirname, '../public' );
 const port = process.env.PORT || 3000;
@@ -8,9 +10,18 @@ console.log(__dirname + '/../public' );
 console.log( publicPath );
 
 var app = express();
+var server = http.createServer( app );
+var io = socketIO( server );
+
+io.on( 'connection', ( socket ) => {
+    console.log( 'New user connected' );
+    socket.on( 'disconnect', () => {
+       console.log( 'User disconnected' );
+    });
+});
 
 app.use( express.static( publicPath ));
 
-app.listen( port, () => {
+server.listen( port, () => {
     console.log( `Chat server is up at port ${port}` );
 });
