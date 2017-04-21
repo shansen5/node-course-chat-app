3a846337ac4640b56ec87bf2114f240b007b4ee4
shansen5 <steven.e.hansen@gmail.com>
2017-04-20 17:55:01 -0700

    Greet new users and alert others.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,14 @@ var io = socketIO( server );
 
 io.on( 'connection', ( socket ) => {
     console.log( 'New user connected' );
+    socket.emit( 'newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+    socket.broadcast.emit( 'newMessage', {
+        from: 'Admin',
+        text: 'A new user joined the chat app'
+    });
     socket.on( 'createMessage', ( message ) => {
         console.log( 'createMessage', message );
         io.emit( 'newMessage', {
@@ -22,6 +30,11 @@ io.on( 'connection', ( socket ) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit(  'newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
     socket.on( 'disconnect', () => {
        console.log( 'User disconnected' );
