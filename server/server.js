85f180d50b8d5e31e7b05d5ebdae25ea8b01b01a
shansen5 <steven.e.hansen@gmail.com>
2017-04-22 21:23:10 -0700

    Add test for callback !== undefined.

--- server/server.js
+++ server/server.js
@@ -24,7 +24,9 @@ io.on( 'connection', ( socket ) => {
         console.log( 'createMessage', message );
         io.emit( 'newMessage', generateMessage(
             message.from, message.text ));
-        callback();
+        if ( callback !== undefined ) {
+            callback();
+        }
     });
     socket.on( 'createLocationMessage', ( coords ) => {
         io.emit( 'newLocationMessage', 
