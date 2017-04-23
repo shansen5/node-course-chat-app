51adbaf9f2e4611a8ebf25799783cc5b946324e6
shansen5 <steven.e.hansen@gmail.com>
2017-04-22 22:13:23 -0700

    Add message scrolling calculation.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,25 @@
 var socket = io();
+
+function scrollToBottom() {
+    // Selectors
+    var messages = jQuery( '#messages' );
+    var newMessage = messages.children( 'li:last-child' );
+    // Heights
+    var clientHeight = messages.prop( 'clientHeight' );
+    var scrollTop = messages.prop( 'scrollTop' );
+    var scrollHeight = messages.prop( 'scrollHeight' );
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    var totalHeight = clientHeight + scrollTop + newMessageHeight + lastMessageHeight;
+    console.log( 'totalHeight = ', totalHeight );
+    console.log( 'scrollHeight = ', scrollHeight );
+
+    if ( totalHeight >= scrollHeight ) {
+        messages.scrollTop( scrollHeight );
+    }
+
+}
+
 socket.on( 'connect', function() {
     console.log( 'Connected to server' );
 });
@@ -14,6 +35,7 @@ socket.on( 'newMessage', function( message ) {
         createdAt: formattedTime
     } );
     jQuery( '#messages' ).append( html );
+    scrollToBottom();
     // var li = jQuery( '<li></li>' );
     // li.text( `${message.from} ${formattedTime}: ${message.text}` );
 
@@ -29,6 +51,7 @@ socket.on( 'newLocationMessage', function( message ) {
         createdAt: formattedTime
     } );
     jQuery( '#messages' ).append( html );
+    scrollToBottom();
     // var li = jQuery( '<li></li>' );
     // var a = jQuery( '<a target="_blank">My location</a>' );
     // li.text( `${message.from} ${formattedTime}: `);
