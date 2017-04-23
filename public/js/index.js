79bea384550d80142f7ceb7312d52ff7f1e0b630
shansen5 <steven.e.hansen@gmail.com>
2017-04-22 20:36:45 -0700

    Add time formatting and mustache.js templates.

--- public/js/index.js
+++ public/js/index.js
@@ -6,20 +6,35 @@ socket.on( 'disconnect', function() {
     console.log( 'Disconnected from server' );
 });
 socket.on( 'newMessage', function( message ) {
-    console.log( 'New message', message );
-    var li = jQuery( '<li></li>' );
-    li.text( `${message.from}: ${message.text}` );
+    var template = jQuery( '#message-template' ).html();
+    var formattedTime = moment( message.createdAt ).format('h:mm a');
+    var html = Mustache.render( template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    } );
+    jQuery( '#messages' ).append( html );
+    // var li = jQuery( '<li></li>' );
+    // li.text( `${message.from} ${formattedTime}: ${message.text}` );
 
-    jQuery( '#messages' ).append( li );
+    // jQuery( '#messages' ).append( li );
 });
 
 socket.on( 'newLocationMessage', function( message ) {
-    var li = jQuery( '<li></li>' );
-    var a = jQuery( '<a target="_blank">My location</a>' );
-    li.text( `${message.from}: `);
-    a.attr( 'href', message.url );
-    li.append( a );
-    jQuery( '#messages' ).append( li );    
+    var template = jQuery( '#location-template' ).html();
+    var formattedTime = moment( message.createdAt ).format('h:mm a');
+    var html = Mustache.render( template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    } );
+    jQuery( '#messages' ).append( html );
+    // var li = jQuery( '<li></li>' );
+    // var a = jQuery( '<a target="_blank">My location</a>' );
+    // li.text( `${message.from} ${formattedTime}: `);
+    // a.attr( 'href', message.url );
+    // li.append( a );
+    // jQuery( '#messages' ).append( li );    
 });
 
 jQuery( '#message-form' ).on( 'submit', function( e ) {
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on( 'connection', ( socket ) => {
         console.log( 'createMessage', message );
         io.emit( 'newMessage', generateMessage(
             message.from, message.text ));
-        // callback();
+        callback();
     });
     socket.on( 'createLocationMessage', ( coords ) => {
         io.emit( 'newLocationMessage', 
